c48d863768f2ad6e99937992ee6005eb7b04622e
Manuel Diaz <manueldl12062003@gmail.com>
2026-04-23 03:08:37 +0200

    fix: changed login command name to link

--- ms-discord/src/main/kotlin/es/daw/parallaxbot/discord/commands/LinkCommand.kt
+++ ms-discord/src/main/kotlin/es/daw/parallaxbot/discord/commands/LinkCommand.kt
@@ -1,25 +1,25 @@
 package es.daw.parallaxbot.discord.commands
 
+import es.daw.parallaxbot.common.config.DiscordConfig
 import es.daw.parallaxbot.discord.bot.ICommand
 import es.daw.parallaxbot.discord.utils.EmbedFactory
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.UUID
 
-class LoginCommand(private val authApiUrl: String) : ICommand {
+class LinkCommand(val config: DiscordConfig) : ICommand {
 
-    override val name: String = "login"
+    override val name: String = "link"
     override val description: String = "Link Discord account with API"
 
-    val logger: Logger = LoggerFactory.getLogger(LoginCommand::class.java)
+    val logger: Logger = LoggerFactory.getLogger(LinkCommand::class.java)
 
     override suspend fun execute(event: SlashCommandInteractionEvent) {
         try {
             event.deferReply(true).queue()
 
             val embed = EmbedFactory.userAuth(event.user.name, event.user.avatarUrl)
-                .addField("Link", authApiUrl, false)
+                .addField("Link", config.authApiUrl, false)
 
             event.hook.sendMessageEmbeds(embed.build())
                 .setEphemeral(true)
@@ -27,7 +27,7 @@ class LoginCommand(private val authApiUrl: String) : ICommand {
                 .join()
         } catch (e: Exception) {
             logger.error(e.message)
-            event.hook.sendMessage("error Sending login link: ${e.message}").submit().join()
+            event.hook.sendMessage("error Sending link: ${e.message}").submit().join()
         }
     }
 }
--- ms-discord/src/main/kotlin/es/daw/parallaxbot/discord/module/CommandModule.kt
+++ ms-discord/src/main/kotlin/es/daw/parallaxbot/discord/module/CommandModule.kt
@@ -2,10 +2,9 @@ package es.daw.parallaxbot.discord.module
 
 import es.daw.parallaxbot.discord.bot.ICommand
 import es.daw.parallaxbot.discord.commands.EventsCommand
-import es.daw.parallaxbot.discord.commands.LoginCommand
+import es.daw.parallaxbot.discord.commands.LinkCommand
 import es.daw.parallaxbot.discord.commands.SetChannelCommand
 import es.daw.parallaxbot.discord.commands.SetDeliveryCommand
-import es.daw.parallaxbot.common.config.DiscordConfig
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -13,14 +12,14 @@ import org.koin.dsl.module
  * Registers slash command implementations and publishes aggregate command list for bot startup.
  */
 val commandModule = module {
-    single { LoginCommand(get<DiscordConfig>().authApiUrl) }
+    singleOf(::LinkCommand)
     singleOf(::EventsCommand)
     singleOf(::SetChannelCommand)
     singleOf(::SetDeliveryCommand)
 
     single<List<ICommand>> {
         listOf(
-            get<LoginCommand>(),
+            get<LinkCommand>(),
             get<EventsCommand>(),
             get<SetChannelCommand>(),
             get<SetDeliveryCommand>()
